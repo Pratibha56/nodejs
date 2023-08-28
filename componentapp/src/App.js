b4cd972e8dcf99bc9741be4e26dab6eb8d014303
pratibha <dudipratibha1234@gmail.com>
2023-08-28 14:56:46 +0530

    done

---
+++ componentapp/src/App.js
@@ -0,0 +1,15 @@
+import AComponent from './AComponent';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <AComponent/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
